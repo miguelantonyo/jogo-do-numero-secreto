4f5a4e24e8f2473189ada5b1fdce27354e690d40
miguelantonyo <miguel.antonyo@gmail.com>
2024-06-02 10:57:02 -0300

    git commit -m "mensagem"

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio(); 
 let tentativas = 1; 
-let luz = 3; 
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
